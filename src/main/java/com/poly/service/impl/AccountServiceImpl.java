112939517c000b7df555ae6ce45746cf06cd18ce
Thanh Tran <tranminhthanha994@gmail.com>
2023-08-17 23:13:52 +0700

    first commit

--- src/main/java/com/poly/service/impl/AccountServiceImpl.java
+++ src/main/java/com/poly/service/impl/AccountServiceImpl.java
@@ -1,8 +1,13 @@
 package com.poly.service.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.poly.dao.AccountDao;
@@ -28,6 +33,61 @@ public class AccountServiceImpl implements AccountService {
 	public Account findByUsername(String username) {
 		// TODO Auto-generated method stub
 		return accdao.findByUsername(username);
+	}
+
+	@Override
+	public <S extends Account> S save(S entity) {
+		return accdao.save(entity);
+	}
+
+	@Override
+	public List<Account> findAll() {
+		return accdao.findAll();
+	}
+
+	@Override
+	public <S extends Account> List<S> saveAll(Iterable<S> entities) {
+		return accdao.saveAll(entities);
+	}
+
+	@Override
+	public <S extends Account> Page<S> findAll(Example<S> example, Pageable pageable) {
+		return accdao.findAll(example, pageable);
+	}
+
+	@Override
+	public long count() {
+		return accdao.count();
+	}
+
+	@Override
+	public void delete(Account entity) {
+		accdao.delete(entity);
+	}
+
+	@Override
+	public void deleteAll() {
+		accdao.deleteAll();
+	}
+
+	@Override
+	public <S extends Account> List<S> findAll(Example<S> example) {
+		return accdao.findAll(example);
+	}
+
+	@Override
+	public <S extends Account> List<S> findAll(Example<S> example, Sort sort) {
+		return accdao.findAll(example, sort);
+	}
+
+	@Override
+	public Account getAccountByEmail(String email) {
+		return accdao.getAccountByEmail(email);
+	}
+
+	@Override
+	public Account findAccountByEmail(String email) {
+		return accdao.findAccountByEmail(email);
 	}
 
 	
